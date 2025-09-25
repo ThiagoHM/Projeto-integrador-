7d2868a19c1ae46802eabb316372506880363cdc
Thiago Henrique Machado <thiago09240@gmail.com>
2025-09-24 22:27:56 -0300

    Delete PROJETOS directory

--- PROJETOS/drawer/src/profile.js
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function ProfileScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>👤 Tela Perfil 👤</Text>  
-    </View>
-  );
-}
